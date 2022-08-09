052d5eea87e3437b9a7fa31e97c4c5ad9bf62bc4
PaingHeinKhant <paingheinkhant.mm@gmail.com>
2022-08-09 12:46:22 +0630

    first commit

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 import './style.scss'
 import * as bootstrap from 'bootstrap';
+import './vendor/slick-1.8.1/slick/slick.min';
 
+
+ $(document).ready(function(){
+     $('.multiple-items').slick({
+         slidesToShow: 3,
+         slidesToScroll: 1,
+         autoplay: true,
+         autoplaySpeed: 1500,
+     });
+});
 
 
