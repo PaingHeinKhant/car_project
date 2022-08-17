68ff677306462b60429aa87c29f8412576f391fb
PaingHeinKhant <paingheinkhant.mm@gmail.com>
2022-08-17 12:40:45 +0630

    second

--- main.js
+++ main.js
@@ -9,7 +9,42 @@ import './vendor/slick-1.8.1/slick/slick.min';
          slidesToScroll: 1,
          autoplay: true,
          autoplaySpeed: 1500,
+         responsive: [
+             {
+                 breakpoint: 1024,
+                 settings: {
+                     slidesToShow: 2,
+                     slidesToScroll: 5,
+                 }
+             },
+             {
+                 breakpoint: 600,
+                 settings: {
+                     slidesToShow: 1,
+                     slidesToScroll: 3
+                 }
+             },
+             {
+                 breakpoint: 480,
+                 settings: {
+                     slidesToShow: 1,
+                     slidesToScroll: 2
+                 }
+             }
+
+         ]
      });
 });
+
+ let carAni = document.getElementById('carAni');
+
+ window.addEventListener('scroll',function (){
+     let value = this.scrollY;
+
+     // console.log(value);
+
+     carAni.style.top = (value-2000) * 0.2 + 'px';
+ })
+
 
 
